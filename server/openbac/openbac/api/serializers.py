d0b6dc85371d9e56811ef9f445d7c46ae4d2c016
Richard Penshorn <richard@penshorn.net>
2017-02-11 20:41:04 -0600

    got auth working with api import

--- server/openbac/openbac/api/serializers.py
+++ server/openbac/openbac/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 import datetime
 
 class AuthSerializer(serializers.Serializer):
-    id = serializers.CharField(max_length=150)
+    card_id = serializers.CharField(max_length=150)
 
 
 class ResponseSerializer(serializers.Serializer):
--- server/openbac/openbac/api/views.py
+++ server/openbac/openbac/api/views.py
@@ -8,7 +8,7 @@ from rest_framework import status
 
 from .serializers import AuthSerializer, ResponseSerializer
 
-from openbac.bac.models import Event, Relay, Action
+from openbac.bac.models import Event, Relay, Action, Cardholder
 
 # Create your views here.
 
@@ -17,19 +17,36 @@ class Auth_request(APIView):
     serializer_class = AuthSerializer
     def post(self, request, format=None):
         serializer = AuthSerializer(data=request.data)
-
+        print serializer
         if serializer.is_valid():
+
+            parsed_data = serializer.data
+            card_id = parsed_data['card_id']
+
+            try:
+                cardholder = Cardholder.objects.get(card_id=card_id)
+            except Cardholder.DoesNotExist:
+                cardholder = None
+
             event = Event()
             event.time = datetime.datetime.now()
             event.reader = request.user.reader
             event.relay = Relay.objects.get(paired_reader=request.user.reader.id)
+            if cardholder:
+
+                event.cardholder = cardholder
+                event.action_taken = event.relay.action
+                event.save()
+                return_data = ResponseSerializer(data={'auth_decision': True, 'led_color': "green"})
+            else:
+                event.action_taken = event.relay.action
+                event.save()
+                return_data = ResponseSerializer(data={'auth_decision': False, 'led_color': "red"})
 
 
-            event.action_taken = event.relay.action
-            event.save()
 
             #add logic code, check if card is allowed, send request to relay
 
-            return_data = ResponseSerializer(data={'auth_decision': True, 'led_color': "green"})
+
             return_data.is_valid()
             return Response(return_data.data, status=status.HTTP_202_ACCEPTED)
